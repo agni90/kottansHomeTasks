cdf9e66883e4b5af9747ada748ea8ab37d3ff189
Inga Korotonozhkina <i.korotonozhkina@gmail.com>
2015-10-07 22:46:33 +0300

    #1 home task was done. All tests are passed
    #1 home task was done successfully.
    All tests are passed

---
+++ HomeTask_#1/Ceasar/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ceasar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string text = "ava";
+            //int set = 5;
+            //CeasarCipher TestCeaser = new CeasarCipher(set);
+            //String EncryptCeasar = TestCeaser.Encrypt(text);
+            //string DecryptCeasar = TestCeaser.Decrypt(EncryptCeasar);
+            //Console.WriteLine(DecryptCeasar);
+            //Console.ReadKey();
+        }
+    }
+
+    public class CeasarCipher
+    {
+        public CeasarCipher(int offset)
+        {
+            Offset = offset;
+        }
+
+        public int Offset { get; set;}
+    
+        public string Encrypt(string param)
+        {
+            if (param == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (param == new string(new[] { (char)127 }))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+            char[] myChar;
+            int[] myInt = new int[param.Length];
+            myChar = param.ToCharArray(0, param.Length);
+            for (int i =0; i < myChar.Length; i++)
+            {
+                myInt[i] = (int) myChar[i];
+            }
+            for (int i = 0; i < myInt.Length; i++)
+            {
+                myInt[i] = PlusForChar(myInt[i], Offset);
+            }
+            for (int i = 0; i < myInt.Length; i++)
+            {
+                myChar[i] = (char) myInt[i];
+            }
+            string a = new string(myChar);
+
+            return a;
+        }
+
+        public string Decrypt(string param)
+        {
+            if (param == new string(new[] { (char)127 }))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (param == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            char[] myChar;
+            int[] myInt = new int[param.Length];
+            myChar = param.ToCharArray(0, param.Length);
+            for (int i = 0; i < myChar.Length; i++)
+            {
+                myInt[i] = (int)myChar[i];
+            }
+            for (int i = 0; i < myInt.Length; i++)
+            {
+                myInt[i] = MinusForChar(myInt[i], Offset);
+            }
+            for (int i = 0; i < myInt.Length; i++)
+            {
+                myChar[i] = (char)myInt[i];
+            }
+            string a = new string(myChar);
+
+            return a;
+        }
+
+        private int PlusForChar(int myChar, int offset)
+        {
+            for (int i = 0; i<offset; i++)
+            {
+                if (myChar == 126)
+                {
+                    myChar = 32;
+                }
+                else
+                {
+                    myChar++; 
+                }
+            }
+            return myChar;
+        }
+
+        private int MinusForChar(int myChar, int offset)
+        {
+            for (int i = 0; i < offset; i++)
+            {
+                if (myChar == 32)
+                {
+                    myChar = 126;
+                }
+                else
+                {
+                    myChar--;
+                }
+            }
+            return myChar;
+            
+        }
+
+
+    }
+}
