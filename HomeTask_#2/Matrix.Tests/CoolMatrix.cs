8a58058b62f603099f848f8b5dffc8f82ec1fa4a
Inga Korotonozhkina <i.korotonozhkina@gmail.com>
2015-10-20 21:47:57 +0300

    Matrix home work part1
    Partial realization of Matrix. All SizeTests are passed

---
+++ HomeTask_#2/Matrix.Tests/CoolMatrix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Matrix.Tests
+{
+    public class CoolMatrix
+    {
+        private int[,] arr;
+        public int width; // first param
+        public int height; // second param
+
+        public CoolMatrix(int[,] arr)
+        {
+            if (arr == null) throw new ArgumentNullException();
+            this.arr = arr;
+            width = arr.GetLength(0);
+            height = arr.GetLength(1);
+
+        }
+
+        public object Size { get; set; }
+        public bool IsSquare => height == width;
+
+        public static implicit operator CoolMatrix(int[,] myArr)
+        {
+            return new CoolMatrix(myArr);
+        }
+    }
+}
---
+++ HomeTask_#2/Matrix.Tests/Size.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework.Constraints;
+
+namespace Matrix.Tests
+{
+    public class Size
+    {
+        private int height;
+        private int width;
+
+        public Size(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Width => width;
+        public int Height => height;
+
+ 
+        public bool IsSquare => width == height;
+
+        public static bool operator ==(Size sizeA, Size sizeB)
+        {
+            return sizeA.height == sizeB.height && sizeA.width ==sizeB.width;
+        }
+
+        public static bool operator !=(Size sizeA, Size sizeB)
+        {
+            return sizeA.height != sizeB.height && sizeA.width != sizeB.width;
+        }
+    }
+}
